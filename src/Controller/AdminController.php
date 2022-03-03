0f435946a68a953d8ab34d45542e24d2dcfe662e
raresnaghi <rares.naghi@yahoo.com>
2022-03-03 18:16:46 +0200

    first commit

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Utils\CategoryTreeAdminList;
+use App\Entity\Category;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * @Route("/admin")
+ */
+class AdminController extends AbstractController
+{
+    /**
+     * @Route("/", name="admin_main_page")
+     */
+    public function index()
+    {
+        return $this->render('admin/my_profile.html.twig');
+    }
+
+
+    /**
+     * @Route("/categories", name="categories")
+     */
+    public function categories(CategoryTreeAdminList $categories)
+    {
+        $categories->getCategoryList($categories->buildTree());
+        return $this->render('admin/categories.html.twig', [
+            'categories' => $categories->categorylist
+        ]);
+    }
+
+    /**
+     * @Route("/edit-category", name="edit_category")
+     */
+    public function editCategory()
+    {
+        return $this->render('admin/edit_category.html.twig');
+    }
+
+    /**
+     * @Route("/delete-category/{id}", name="delete_category")
+     */
+    public function deleteCategory(Category $category)
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->remove($category);
+        $entityManager->flush();
+        return $this->redirectToRoute('categories');
+    }
+
+    /**
+     * @Route("/videos", name="videos")
+     */
+    public function videos()
+    {
+        return $this->render('admin/videos.html.twig');
+    }
+
+    /**
+     * @Route("/upload-video", name="upload_video")
+     */
+    public function uploadVideo()
+    {
+        return $this->render('admin/upload_video.html.twig');
+    }
+
+    /**
+     * @Route("/users", name="users")
+     */
+    public function users()
+    {
+        return $this->render('admin/users.html.twig');
+    }
+}
